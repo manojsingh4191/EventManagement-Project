c84e7225cd0c7e629c4796d83d19fbe034f1ed42
manoj singh <manojsingh76543212@gmail.com>
2026-07-10 22:22:14 +0530

    4

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,6 @@ app.use(
     credentials: true,
   })
 );
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
-    });
-}
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
